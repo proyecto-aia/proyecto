e5c0487892d4fca4e778f87698ab5374ff092c2b
proyecto-aia <el.viejo.martin.webmaster@gmail.com>
2015-06-04 21:07:39 -0300

    Modificar el nombre de la base de datos a la que se conecta.

--- app/Config/database.php
+++ app/Config/database.php
@@ -74,7 +74,7 @@ class DATABASE_CONFIG {
 		'host' => 'localhost',
 		'login' => 'root',
 		'password' => 'fedora',
-		'database' => 'proyecto-aia',
+		'database' => 'proyecto',
 		'prefix' => '',
 		'encoding' => 'utf8',
 	);
