d57fc84d010dfdd40e4eda632cfca3eabc986450
proyecto-aia <el.viejo.martin.webmaster@gmail.com>
2015-06-05 20:11:07 -0300

    Con .gitignore nuevamente comentado 2.

--- app/Config/database.php
+++ app/Config/database.php
@@ -73,7 +73,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => 'fedora',
+		'password' => '',
 		'database' => 'proyecto',
 		'prefix' => '',
 		'encoding' => 'utf8',
